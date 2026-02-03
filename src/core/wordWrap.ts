5f14b2e7cdc0221c0c1f8620b5d2629e8c7d31c4
Luis <luis.amez.vitoria@gmail.com>
2026-02-03 18:33:51 +0100

    Implement functionality with primitive types

--- src/core/wordWrap.ts
+++ src/core/wordWrap.ts
@@ -1,4 +1,20 @@
 export function wordWrap(text: string, columnWidth: number) {
-  console.log(columnWidth);
-  return text;
+  if (columnWidth <= 0) throw new Error('columnWidth must be greater than 0');
+  if (text.length <= columnWidth) return text;
+
+  let wrapIndex = getWrapIndex(text, columnWidth);
+  let wrappedText = text.substring(0, wrapIndex).trim();
+  let unwrappedText = text.substring(wrapIndex);
+  while (unwrappedText.length > columnWidth) {
+    wrapIndex = getWrapIndex(unwrappedText, columnWidth);
+    wrappedText += '\n' + unwrappedText.substring(0, wrapIndex).trim();
+    unwrappedText = unwrappedText.substring(wrapIndex);
+  }
+  return wrappedText + '\n' + unwrappedText;
+}
+
+function getWrapIndex(text: string, columnWidth: number) {
+  const textToWrap = text.substring(0, columnWidth);
+  const whiteSpaceIndex = textToWrap.indexOf(' ');
+  return textToWrap.includes(' ') ? whiteSpaceIndex + 1 : columnWidth;
 }
--- src/tests/wordWrap.test.ts
+++ src/tests/wordWrap.test.ts
@@ -1,9 +1,53 @@
 import { wordWrap } from '../core/wordWrap';
+import { expect } from '@jest/globals';
 
 describe('wordWrap', () => {
-  it('does nothing for an empty string', () => {
+  it('does not wrap an empty string', () => {
     const result = wordWrap('', 5);
 
     expect(result).toBe('');
+  });
+
+  it('does not wrap a text shorter than the column width', () => {
+    const result = wordWrap('hello', 5);
+
+    expect(result).toBe('hello');
+  });
+
+  it('wraps a text longer than the column width', () => {
+    const result = wordWrap('longword', 4);
+
+    expect(result).toBe('long\nword');
+  });
+
+  it('wraps multiple times a text longer than twice the column width', () => {
+    const result = wordWrap('reallylongword', 4);
+
+    expect(result).toBe('real\nlylo\nngwo\nrd');
+  });
+
+  it('deletes the white space if it wraps in a space', () => {
+    const result = wordWrap('abc def', 4);
+
+    expect(result).toBe('abc\ndef');
+  });
+
+  it('deletes multiple white spaces if it wraps in multiple spaces', () => {
+    const result = wordWrap('abc def ghi', 4);
+
+    expect(result).toBe('abc\ndef\nghi');
+  });
+
+  it('prioritizes wrapping in a space over the middle of a word', () => {
+    expect(wordWrap(' abcd', 4)).toBe('\nabcd');
+    expect(wordWrap('ab cde fg', 4)).toBe('ab\ncde\nfg');
+  });
+
+  it('fails if the column width is negative', () => {
+    expect(() => wordWrap('hello', -1)).toThrow('columnWidth must be greater than 0');
+  });
+
+  it('fails if the column width is zero', () => {
+    expect(() => wordWrap('hello', 0)).toThrow('columnWidth must be greater than 0');
   });
 });
