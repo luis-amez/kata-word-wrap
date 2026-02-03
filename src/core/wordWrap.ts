1b2fa27cf5f75f95cc965cf09a20a9a1f0a9c2f4
Luis <luis.amez.vitoria@gmail.com>
2026-02-03 19:48:24 +0100

    Refactor to use value objects

--- src/core/wordWrap.ts
+++ src/core/wordWrap.ts
@@ -1,20 +1,56 @@
-export function wordWrap(text: string, columnWidth: number) {
-  if (columnWidth <= 0) throw new Error('columnWidth must be greater than 0');
-  if (text.length <= columnWidth) return text;
-
-  let wrapIndex = getWrapIndex(text, columnWidth);
-  let wrappedText = text.substring(0, wrapIndex).trim();
-  let unwrappedText = text.substring(wrapIndex);
-  while (unwrappedText.length > columnWidth) {
-    wrapIndex = getWrapIndex(unwrappedText, columnWidth);
-    wrappedText += '\n' + unwrappedText.substring(0, wrapIndex).trim();
-    unwrappedText = unwrappedText.substring(wrapIndex);
+export class ColumnWidth {
+  private constructor(private readonly value: number) {
+    this.value = value;
   }
-  return wrappedText + '\n' + unwrappedText;
+
+  static of(value: number) {
+    if (value <= 0) throw new Error('columnWidth must be greater than 0');
+    return new ColumnWidth(value);
+  }
+
+  getValue() {
+    return this.value;
+  }
 }
 
-function getWrapIndex(text: string, columnWidth: number) {
-  const textToWrap = text.substring(0, columnWidth);
-  const whiteSpaceIndex = textToWrap.indexOf(' ');
-  return textToWrap.includes(' ') ? whiteSpaceIndex + 1 : columnWidth;
+export class WrappableText {
+  private constructor(private readonly text: string) {
+    this.text = text;
+  }
+
+  static of(text: string) {
+    // This should never happen, undefined and null are not assignable to string
+    if (text === undefined || text === null) return new WrappableText('');
+    return new WrappableText(text);
+  }
+
+  wrapText(columnWidth: ColumnWidth) {
+    if (this.getText().length <= columnWidth.getValue()) return this.getText();
+
+    let wrappedText = this.getWrappedText(this.getText(), columnWidth);
+    let unwrappedText = this.getUnwrappedText(this.getText(), columnWidth);
+    while (unwrappedText.length > columnWidth.getValue()) {
+      wrappedText += '\n' + this.getWrappedText(unwrappedText, columnWidth);
+      unwrappedText = this.getUnwrappedText(unwrappedText, columnWidth);
+    }
+    return wrappedText + '\n' + unwrappedText;
+  }
+
+  private getText() {
+    return this.text;
+  }
+
+  private getWrapIndex(text: string, columnWidth: ColumnWidth) {
+    const textToWrap = text.substring(0, columnWidth.getValue());
+    const whiteSpaceIndex = textToWrap.indexOf(' ');
+    return textToWrap.includes(' ') ? whiteSpaceIndex + 1 : columnWidth.getValue();
+  }
+
+  private getWrappedText(text: string, columnWidth: ColumnWidth) {
+    return text.substring(0, this.getWrapIndex(text, columnWidth)).trim();
+  }
+
+  private getUnwrappedText(text: string, columnWidth: ColumnWidth) {
+    return text.substring(this.getWrapIndex(text, columnWidth));
+  }
 }
--- src/tests/wordWrap.test.ts
+++ src/tests/wordWrap.test.ts
@@ -1,53 +1,53 @@
-import { wordWrap } from '../core/wordWrap';
+import { ColumnWidth, WrappableText } from '../core/wordWrap';
 import { expect } from '@jest/globals';
 
 describe('wordWrap', () => {
   it('does not wrap an empty string', () => {
-    const result = wordWrap('', 5);
+    const result = WrappableText.of('').wrapText(ColumnWidth.of(5));
 
     expect(result).toBe('');
   });
 
   it('does not wrap a text shorter than the column width', () => {
-    const result = wordWrap('hello', 5);
+    const result = WrappableText.of('hello').wrapText(ColumnWidth.of(5));
 
     expect(result).toBe('hello');
   });
 
   it('wraps a text longer than the column width', () => {
-    const result = wordWrap('longword', 4);
+    const result = WrappableText.of('longword').wrapText(ColumnWidth.of(4));
 
     expect(result).toBe('long\nword');
   });
 
   it('wraps multiple times a text longer than twice the column width', () => {
-    const result = wordWrap('reallylongword', 4);
+    const result = WrappableText.of('reallylongword').wrapText(ColumnWidth.of(4));
 
     expect(result).toBe('real\nlylo\nngwo\nrd');
   });
 
   it('deletes the white space if it wraps in a space', () => {
-    const result = wordWrap('abc def', 4);
+    const result = WrappableText.of('abc def').wrapText(ColumnWidth.of(4));
 
     expect(result).toBe('abc\ndef');
   });
 
   it('deletes multiple white spaces if it wraps in multiple spaces', () => {
-    const result = wordWrap('abc def ghi', 4);
+    const result = WrappableText.of('abc def ghi').wrapText(ColumnWidth.of(4));
 
     expect(result).toBe('abc\ndef\nghi');
   });
 
   it('prioritizes wrapping in a space over the middle of a word', () => {
-    expect(wordWrap(' abcd', 4)).toBe('\nabcd');
-    expect(wordWrap('ab cde fg', 4)).toBe('ab\ncde\nfg');
+    expect(WrappableText.of(' abcd').wrapText(ColumnWidth.of(4))).toBe('\nabcd');
+    expect(WrappableText.of('ab cde fg').wrapText(ColumnWidth.of(4))).toBe('ab\ncde\nfg');
   });
 
   it('fails if the column width is negative', () => {
-    expect(() => wordWrap('hello', -1)).toThrow('columnWidth must be greater than 0');
+    expect(() => WrappableText.of('hello').wrapText(ColumnWidth.of(-1))).toThrow('columnWidth must be greater than 0');
   });
 
   it('fails if the column width is zero', () => {
-    expect(() => wordWrap('hello', 0)).toThrow('columnWidth must be greater than 0');
+    expect(() => WrappableText.of('hello').wrapText(ColumnWidth.of(0))).toThrow('columnWidth must be greater than 0');
   });
 });
